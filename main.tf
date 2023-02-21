272935147c90919d1576522d942865159ffeb5d8
Raghu K <raghuk.vit@gmail.com>
2023-02-21 07:11:29 +0530

    Update tf-module-alb/

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group" "MAIN" {
+  name        = "${var.env}-alb-${var.subnets_name}-security-group"
+  description = "${var.env}-alb-${var.subnets_name}-security-group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allow_cidr
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-alb-${var.subnets_name}-security-group" }
+  )
+}
+
+
